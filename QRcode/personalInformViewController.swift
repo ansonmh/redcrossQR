dd06819326d5381179ca4efd9331a111ba54faab
ansonmh <ansonmh@gmail.com>
2018-03-28 02:49:53 +0800

    KO detail

--- QRcode/personalInformViewController.swift
+++ QRcode/personalInformViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import Firebase
 
 class personalInformViewController: UIViewController {
+    @IBOutlet weak var UID: UITextField!
     @IBOutlet weak var fName: UITextField!
     @IBOutlet weak var lname: UITextField!
     @IBOutlet weak var chName: UITextField!
@@ -19,22 +20,74 @@ class personalInformViewController: UIViewController {
     @IBOutlet weak var Height: UITextField!
     @IBOutlet weak var bGroup: UITextField!
     @IBOutlet weak var DID: UITextField!
-    @IBOutlet weak var HKhave: UISegmentedControl!
+
+    @IBOutlet weak var hkHave: UITextField!
+    @IBOutlet weak var from: UITextField!
     @IBOutlet weak var lastDdate: UITextField!
     
     var urlName:String?
-    
+    var ref: DatabaseReference!
     
     override func viewDidLoad() {
       
        
 
-      
+        UID.text = urlName
         super.viewDidLoad()
-            fName.text = urlName
+       
+       
+      
+        
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func loadData(_ sender: UIButton) {
+        ref = Database.database().reference()
+        ref.child("user").child(UID.text!).observeSingleEvent(of: .value, with: { (snapshot) in
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                let key = snap.key
+                var value = snap.value
+                switch key {
+                case ("firstName"):
+                    self.fName.text = "\(value!)"
+                case ("blood_group"):
+                    self.bGroup.text = "\(value!)"
+                case ("chinese_name"):
+                    self.chName.text = "\(value!)"
+                case ("DOB"):
+                    self.chName.text = "\(value!)"
+                case ("HKID"):
+                    self.HKID.text = "\(value!)"
+                case ("height"):
+                    self.Height.text = "\(value!)"
+                case ("finishQC"):
+                    let ram = "\(value!)"
+                    if (ram == "1"){
+                    self.from.text = "Yes"
+                    }else{
+                        self.from.text = "No"
+                    }
+                case ("whether_donated_blood"):
+                    self.hkHave.text = "\(value!)"
+                case ("date_brith"):
+                    self.DOB.text = "\(value!)"
+                case ("lastName"):
+                    self.lname.text = "\(value!)"
+                case ("weight"):
+                    self.weight.text = "\(value!)"
+                case ("last_donation_date"):
+                    self.lastDdate.text = "\(value!)"
+                default:
+                    break
+                }
+                
+                print("key = \(key)  value = \(value!)")
+            }
+        })
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
